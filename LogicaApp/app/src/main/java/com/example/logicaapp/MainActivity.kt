7747232fd86baf5b23e6a71b0519fdd88951250e
IrisSaskia <iris.oostra@hva.nl>
2020-02-13 15:08:52 +0100

    Array probleem opgelost

--- LogicaApp/app/src/main/java/com/example/logicaapp/MainActivity.kt
+++ LogicaApp/app/src/main/java/com/example/logicaapp/MainActivity.kt
@@ -9,16 +9,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-   /* private val valuesA = intArrayOf(R.id.tvA1, R.id.tvA2, R.id.tvA3, R.id.tvA4);
-    private val valuesB = intArrayOf(R.id.tvB1, R.id.tvB2, R.id.tvB3, R.id.tvB4);
-    private var answers = intArrayOf(R.id.etAnswer1, R.id.etAnswer2, R.id.etAnswer3, R.id.etAnswer4);
-    private val amountOfQuestions = 4;*/
-    private var amountCorrect: Int = 0;
-    private var returnMessage: String = getString(R.string.super_error);
+    lateinit var valuesA: Array<TextView>;
+    lateinit var valuesB: Array<TextView>;
+    lateinit var answers: Array<EditText>;
+    var amountOfQuestions: Int = 5;
+    var amountCorrect: Int = 0;
+    lateinit var returnMessage: String;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        valuesA = arrayOf(
+            findViewById(R.id.tvA1),
+            findViewById(R.id.tvA2),
+            findViewById(R.id.tvA3),
+            findViewById(R.id.tvA4));
+        valuesB = arrayOf(
+            findViewById(R.id.tvB1),
+            findViewById(R.id.tvB2),
+            findViewById(R.id.tvB3),
+            findViewById(R.id.tvB4));
+        answers = arrayOf(
+            findViewById(R.id.etAnswer1),
+            findViewById(R.id.etAnswer2),
+            findViewById(R.id.etAnswer3),
+            findViewById(R.id.etAnswer4));
+        returnMessage = getString(R.string.super_error);
 
         initViews();
     }
@@ -27,10 +44,13 @@ class MainActivity : AppCompatActivity() {
         btnSubmit.setOnClickListener {
             if(etAnswer1.text.isNotEmpty() && etAnswer2.text.isNotEmpty() && etAnswer3.text.isNotEmpty() && etAnswer4.text.isNotEmpty()) {
                 amountCorrect = 0;
-                /*for(x in 0..amountOfQuestions) {
-                    var valueForA = findViewById<TextView>(valuesA[x]).text.toString();
-                    var valueForB = findViewById<TextView>(valuesB[x]).text.toString();
-                    var answerValue = findViewById<EditText>(answers[x]).text.toString();
+                for(x in 1..amountOfQuestions) {
+                    var valueForA = valuesA[x].text.toString();
+                    var valueForB = valuesB[x].text.toString();
+                    var answerValue = answers[x].text.toString();
+                    //var valueForA = findViewById<TextView>(valuesA[x]).text.toString();
+                    //var valueForB = findViewById<TextView>(valuesB[x]).text.toString();
+                    //var answerValue = findViewById<EditText>(answers[x]).text.toString();
                     //var valueForA = "T";
                     //var valueForB = "T";
                     //var answerValue = "T";
@@ -40,7 +60,7 @@ class MainActivity : AppCompatActivity() {
                     if(checkIfCorrect(valueForA, valueForB, answerValue)) {
                         amountCorrect++;
                     }
-                }*/
+                }
                 returnMessage = getString(R.string.check_message, amountCorrect);
             } else {
                 returnMessage = getString(R.string.error_message);
